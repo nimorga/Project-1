3ece1c3efbf1547e6d09825540ce0d93511f4260
Nicole <nimorga@siue.edu>
2023-09-19 20:03:29 -0500

    updated code

--- getURL.py
+++ getURL.py
@@ -1,7 +1,7 @@
 import requests #imported from miniconda
 import sys #included in STD library
 
-outputFile = open("URL.txt","w")
+outputFile = open("URL.txt","w",encoding="utf-8")
 url = sys.argv[1]
 
 getURL=requests.get(url)
